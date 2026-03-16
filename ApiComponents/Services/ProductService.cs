25ae3190a8c5cdfcd7d6ffa47108cd9faa3f67f2
claudiocds1987 <cla8787@gmail.com>
2026-03-16 12:00:30 -0300

    upload ProductServices

--- ApiComponents/Services/ProductService.cs
+++ ApiComponents/Services/ProductService.cs
@@ -40,14 +40,27 @@ public class ProductService(IProductRepository productRepo, AppDbContext context
                 // 3. Si todo está bien, lo agregamos a la lista
                 productsToSave.Add(new Product
                 {
-                    title = title,
+                    title = reader.GetValue(1)?.ToString()?.Trim() ?? string.Empty,
                     description = reader.GetValue(2)?.ToString()?.Trim() ?? string.Empty,
                     price = Convert.ToDecimal(reader.GetValue(3)),
+                    discountPercentage = Convert.ToDecimal(reader.GetValue(4)),
+                    rating = Convert.ToDecimal(reader.GetValue(5)),
                     stock = Convert.ToInt32(reader.GetValue(6)),
                     sku = reader.GetValue(7)?.ToString()?.Trim() ?? string.Empty,
+                    weight = Convert.ToDecimal(reader.GetValue(8)),
+                    width = Convert.ToDecimal(reader.GetValue(9)),
+                    height = Convert.ToDecimal(reader.GetValue(10)),
+                    depth = Convert.ToDecimal(reader.GetValue(11)),
+                    warrantyInformation = reader.GetValue(12)?.ToString()?.Trim() ?? string.Empty,
+                    shippingInformation = reader.GetValue(13)?.ToString()?.Trim() ?? string.Empty,
+                    availabilityStatus = reader.GetValue(14)?.ToString()?.Trim() ?? string.Empty,
+                    returnPolicy = reader.GetValue(15)?.ToString()?.Trim() ?? string.Empty,
+                    minimumOrderQuantity = Convert.ToInt32(reader.GetValue(16)),
                     thumbnail = reader.GetValue(17)?.ToString()?.Trim() ?? string.Empty,
-                    categoryId = catId,
-                    brandId = brId
+
+                    // IDs estrictos que validaste previamente
+                    categoryId = catId, // Índice 18
+                    brandId = brId      // Índice 19
                 });
             }
         }
