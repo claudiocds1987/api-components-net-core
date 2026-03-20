00eff79f58530eae341c59e38480d832b57dbf5f
claudiocds1987 <cla8787@gmail.com>
2026-03-20 18:23:58 -0300

    upload Dbseeder

--- ApiComponents/Persistence/Seed/DbSeeder.cs
+++ ApiComponents/Persistence/Seed/DbSeeder.cs
@@ -56,35 +56,29 @@ namespace ApiComponents.Persistence.Seed
             {
                 var brandNames = new List<string>
         {
-            // Tecnología y Smartphones
-            "Apple", "Samsung", "Huawei", "Oppo", "Vivo", "Microsoft Surface", "HP Pavilion",
-            "Infinix", "Iphone", "OnePlus 12", "Xiaomi", "Motorola", "Sony", "Asus", "Realme",
-            "Nothing", "Honor", "Redmi", "Poco", "Tecno", "ZTE", "Meizu", "Lenovo", "Razer",
-            "Nokia", "Fairphone", "Gigabyte", "HP", "Microsoft", "Dell", "Acer", "Alienware",
-            "LG", "Fujitsu", "Panasonic", "Dynabook", "System76", "Framework", "Purism",
-
-            // Belleza, Perfumes y Cuidado Personal
-            "L'Oreal Paris", "Essence", "Glamour Beauty", "Velvet Touch", "Chanel", "Dior",
-            "Gucci", "Versace", "Armani Code", "Bvlgari", "Hugo Boss", "Jean Paul Gaultier",
-            "Paco Rabanne", "Prada", "Ralph Lauren", "Yves Saint Laurent",
-
-            // Relojería y Lujo
-            "Rolex", "Casio", "Fossil", "Luxury Watch", "Garmin", "Fitbit", "Amazfit",
-            "Omega", "Tissot", "Seiko", "Longines", "IWC", "Breitling", "Cartier",
+            "Apple", "Samsung", "Huawei", "Oppo", "Vivo", "Microsoft Surface",
+            "HP Pavilion", "Infinix", "L'Oreal Paris", "Essence", "Glamour Beauty",
+            "Velvet Touch", "Chanel", "Dior", "Gucci", "Furniture Co", "Knoll",
+            "Bath Trends", "Home Decor", "Rolex", "Casio", "Fossil", "Luxury Watch",
+            "Fashion Trends", "Annibale Colombo", "Calvin Klein", "Nike", "Adidas",
+            "Puma", "Nescafe", "Versace", "Armani Code", "Bvlgari", "Hugo Boss",
+            "Jean Paul Gaultier", "Paco Rabanne", "Prada", "Ralph Lauren",
+            "Yves Saint Laurent", "Iphone", "OnePlus", "Xiaomi", "Motorola",
+            "Sony", "Asus", "Realme", "Nothing", "Honor", "Redmi", "Poco",
+            "Tecno", "ZTE", "Meizu", "Lenovo", "Razer", "Nokia", "Fairphone",
+            "Generic", "Luxury", "Garmin", "Fitbit", "Amazfit", "Omega",
+            "Tissot", "Seiko", "Longines", "IWC", "Breitling", "Cartier",
             "Audemars", "Patek Philippe Calatrava", "Daniel Wellington", "Citizen",
             "Bulova", "Hamilton", "Movado", "Tudor", "Panerai", "Zenith",
-            "Jaeger-LeCoultre", "Girard-Perregaux", "Vacheron Constantin", "Piaget",
-            "Chopard", "Blancpain", "Breguet", "Glashutte",
-
-            // Hogar, Muebles y Decoración
-            "Furniture Co.", "Knoll", "Bath Trends", "Home Decor", "Annibale Colombo",
-            "Blue & Black",
-
-            // Moda y Otros
-            "Fashion Trends", "Calvin Klein", "Nike", "Adidas", "Puma", "Nescafe", "Generic", "Luxury"
+            "Jaeger-LeCoultre", "Girard-Perregaux", "Vacheron Constantin",
+            "Piaget", "Chopard", "Blancpain", "Breguet", "Glashutte",
+            "Blue & Black", "Gigabyte", "HP", "Microsoft", "Dell", "Acer",
+            "Alienware", "LG", "Fujitsu", "Panasonic", "Dynabook", "System76",
+            "Framework", "Purism", "MSI", "Hublot", "MVMT", "TAG Heuer",
+            "Google", "Dolce & Gabbana", "Nail Couture", "Chic Cosmetics"
         };
 
-                // Eliminamos duplicados por si acaso y convertimos a objetos ProductBrand
+                // Distinct para evitar errores de índice único y Select para crear los objetos
                 var brandsToInsert = brandNames
                     .Distinct()
                     .Select(name => new ProductBrand { name = name })
